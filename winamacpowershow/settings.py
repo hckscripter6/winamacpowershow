a1fc3f9cd5f5fb9b68c4a24ee57f4cba6ccdb6ad
Hunter Krieger <hunterk31@hotmail.com>
2020-04-26 12:30:21 -0400

    add allowed hosts

--- winamacpowershow/settings.py
+++ winamacpowershow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4b8(-y^oavl2#wlp+c3!pcfqu$&9r+=kt+sh$nnfpmvrj9^%_*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'winamacpowershow.herokuapp.com']
 
 
 # Application definition
